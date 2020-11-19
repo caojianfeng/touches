1a25389ee385608e63a8aa809be9f49b676ae7d0
曹建峰 <caojianfeng@innobuddy.com>
2020-11-19 15:55:30 +0800

    Initial Commit

---
+++ touches/touchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchesApp.swift
+//  touches
+//
+//  Created by admin on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct touchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
